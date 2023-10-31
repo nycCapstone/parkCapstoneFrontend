825314c27b219517f02999dc3468d9f197878247
zazimov <zalmanazimov@pursuit.org>
2023-10-31 10:53:29 -0400

    created landing page and added images

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,48 +1,76 @@
+import React from "react";
+import { carvaletlogo } from "../../assets";
+import { Link } from "react-router-dom";
+
 function Nav() {
   return (
-    <>
-      <div className="bg-body-tertiary">
-        <div>
-          <div href="#home">Brand link Hibon</div>
+    <nav className="navbar">
+      <div className="left-nav">
+        <div className="nav-links">
+          <Link to="/about">About Us</Link>
+          <Link to="/login">Log in/Sign up</Link>
         </div>
       </div>
-      <br />
-      <div className="bg-body-tertiary">
-        <div>
-          <div>Brand text</div>
-        </div>
+      <div className="right-nav">
+        <Link to="/">
+          <img src={carvaletlogo} alt="carvaletlogo" className="logo" />
+        </Link>
       </div>
-      <br />
-      <div className="bg-body-tertiary">
-        <div>
-          <div href="/admin">
-            <img
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Dashboard"
-            />
-          </div>
-        </div>
-      </div>
-      <br />
-      <div className="bg-body-tertiary">
-        <div>
-          <a href="/login">
-            <img
-              alt=""
-              src="/img/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Login
-          </a>
-        </div>
-      </div>
-    </>
+    </nav>
   );
 }
 
 export default Nav;
+
+// import React from "react";
+// import { carvaletlogo } from "../../assets";
+// import { Link } from "react-router-dom";
+
+// function Nav() {
+//   return (
+//     <>
+//       <div className="bg-body-tertiary">
+//         <div>
+//           <div href="#home">Brand link</div>
+//         </div>
+//       </div>
+//       <br />
+//       <div className="bg-body-tertiary">
+//         <div>
+//           <div>Brand text</div>
+//         </div>
+//       </div>
+//       <br />
+//       <div className="bg-body-tertiary">
+//         <div>
+//           <div href="/admin">
+//             <img
+//               src="/img/logo.svg"
+//               width="30"
+//               height="30"
+//               className="d-inline-block align-top"
+//               alt="Dashboard"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//       <br />
+//       <div className="bg-body-tertiary">
+//         <div>
+//           <a href="/login">
+//             <img
+//               alt=""
+//               src="/img/logo.svg"
+//               width="30"
+//               height="30"
+//               className="d-inline-block align-top"
+//             />{" "}
+//             Login
+//           </a>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Nav;
