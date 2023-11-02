a6e005ad92325a611eeea545ec900bc7742a1fc3
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-11-02 13:53:58 -0400

    new style for admin

---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./AdminPage.css";
+
+function AdminPage({ userInfo }) {
+  return (
+    <div className="admin-page">
+      <h1>Admin Page</h1>
+      <div className="user-info">
+        <h2>User Information</h2>
+        <p>ID: {userInfo.id}</p>
+        <p>
+          Name: {userInfo.first_name} {userInfo.last_name}
+        </p>
+        <p>Address: {userInfo.address}</p>
+        <p>Email: {userInfo.email}</p>
+        <p>PMT Verified: {userInfo.pmt_verified ? "Yes" : "No"}</p>
+        <p>
+          Client Background Verified:{" "}
+          {userInfo.client_background_verified ? "Yes" : "No"}
+        </p>
+        <div className="roles">
+          <h3>Roles</h3>
+          <div className="role-list">
+            <div className="role-item">
+              <h4>Client</h4>
+              <p>Background: {userInfo.roles.Client.bckgr ? "Yes" : "No"}</p>
+              <p>PMT: {userInfo.roles.Client.pmt ? "Yes" : "No"}</p>
+            </div>
+            <div className="role-item">
+              <h4>ClientOnly</h4>
+              <p>{userInfo.roles.ClientOnly ? "Yes" : "No"}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminPage;
--- src/components/Dashboard/Admin.js
+++ src/components/Dashboard/Admin.js
@@ -1,20 +1,42 @@
 import { Link } from "react-router-dom";
-import User from "../User";
+import React from "react";
 import { useGetUserInfoQuery } from "../../redux/userActions/userApiSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRetweet } from "@fortawesome/free-solid-svg-icons"
+// import { faRetweet } from "@fortawesome/free-solid-svg-icons";
+import AdminPage from "../AdminPage/AdminPage";
+import "./Admin.css";
+// import User from "../User";
 
 const Admin = () => {
-    const { refetch } = useGetUserInfoQuery();
+  const { data, refetch, isLoading, isError } = useGetUserInfoQuery();
 
-    return <section>
-      <h1>Admin Page</h1>
-      <FontAwesomeIcon onClick={()=> refetch()} icon={faRetweet} style={{cursor: 'pointer'}}/>
-        <User/>
+  if (isLoading) {
+    return <div>Loading user information...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h3>Error fetching user information</h3>
+        <button onClick={() => refetch()}>Retry</button>
+      </div>
+    );
+  }
+
+  return (
+    <section>
+      <AdminPage userInfo={data} />
+      <FontAwesomeIcon
+        onClick={() => refetch()}
+        // icon={faRetweet}
+        style={{ cursor: "pointer" }}
+      />
+      {/* <User /> */}
       <div className="flexGrow">
         <Link to="/home">Home</Link>
       </div>
     </section>
+  );
 };
 
 export default Admin;
