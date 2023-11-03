835778cf983250b71ba35431a495b970d74b6f8e
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-11-03 10:51:02 -0400

    new changes for admin

--- src/components/Dashboard/Admin.js
+++ src/components/Dashboard/Admin.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { useGetUserInfoQuery } from "../../redux/userActions/userApiSlice";
+import useLogout from "../../hooks/useLogout";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faRetweet } from "@fortawesome/free-solid-svg-icons";
 import AdminPage from "../AdminPage/AdminPage";
@@ -8,7 +9,14 @@ import "./Admin.css";
 // import User from "../User";
 
 const Admin = () => {
-  const { data, refetch, isLoading, isError } = useGetUserInfoQuery();
+  const { data, refetch, isLoading, isError, isSuccess } =
+    useGetUserInfoQuery();
+
+  const logout = useLogout();
+
+  const signOut = async () => {
+    await logout();
+  };
 
   if (isLoading) {
     return <div>Loading user information...</div>;
@@ -23,20 +31,45 @@ const Admin = () => {
     );
   }
 
-  return (
-    <section>
-      <AdminPage userInfo={data} />
-      {/* <FontAwesomeIcon
-        onClick={() => refetch()}
-        // icon={faRetweet}
-        style={{ cursor: "pointer" }}
-      /> */}
-      {/* <User /> */}
-      <div className="flexGrow">
-        <Link to="/home">Home</Link>
-      </div>
-    </section>
-  );
+  if (isSuccess) {
+    return (
+      <section>
+        <AdminPage userInfo={data} />
+        <div className="flexGrow">
+          {/* <Link to="/home" className="home-link">
+            Home
+          </Link> */}
+        </div>
+        <section className="home-container">
+          <h1 className="welcome-heading">Welcome Home</h1>
+          {data?.email && <p className="user-email">Email: {data?.email}</p>}
+
+          <div className="link-container">
+            <Link to="/client" className="nav-link">
+              Go to the Client page
+            </Link>
+            <Link to="/renter" className="nav-link">
+              Go to the Renter
+            </Link>
+            <Link to="/admin" className="nav-link">
+              Go to the Admin page
+            </Link>
+            <Link to="/go" className="nav-link">
+              Go to the link page
+            </Link>
+          </div>
+
+          <div className="flexGrow">
+            <button className="sign-out-button" onClick={signOut}>
+              Sign Out
+            </button>
+          </div>
+        </section>
+      </section>
+    );
+  }
+
+  return null;
 };
 
 export default Admin;
