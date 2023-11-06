a96a8948ecb4d61f6f08a68dec4ea6b31113c25b
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-11-06 12:49:28 -0500

    Fixed some problems

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -36,34 +36,6 @@ function Nav() {
         </div>
       </div>
     </nav>
-    // <nav className="navbar">
-    //   <div className="left-nav">
-    //     <Link to="/">
-    //       <img className="logo" src={carvaletlogo} alt="logo" />
-    //     </Link>
-    //   </div>
-    //   <div className="right-nav">
-    //     <div className="nav-links">
-    //       <Link className="about" to="/about">
-    //         About Us
-    //       </Link>
-    //       {isLoading && <p>Loading...</p>}
-    //       {isSuccess && (
-    //         <span className="greeting">Hi {userInfo.first_name}</span>
-    //       )}
-    //       {!isSuccess && !isLoading && (
-    //         <>
-    //           <Link className="log-in" to="/login">
-    //             Log in
-    //           </Link>
-    //           <Link className="sign-up" to="/register">
-    //             Sign up
-    //           </Link>
-    //         </>
-    //       )}
-    //     </div>
-    //   </div>
-    // </nav>
   );
 }
 
