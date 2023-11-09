4684370cbb8c7b236c4eccf54550c0893574cdf6
zazimov <zalmanazimov@pursuit.org>
2023-11-09 12:00:59 -0500

    added bookingform, purchasePreview, and purchaseConfired components

---
+++ src/components/PurchaseConfirmed/PurchaseConfirmed.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PurchaseConfirmed() {
+  return <div>PurchaseConfirmed</div>;
+}
+
+export default PurchaseConfirmed;
---
+++ src/components/PurchasePreview/PurchasePreview.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PurchasePreview() {
+  return <div>PurchasePreview</div>;
+}
+
+export default PurchasePreview;
