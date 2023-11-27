5ba5984d50d59dc27cd04b2f24eef65c20723342
YasserS-21 <saadi.yasser04@gmail.com>
2023-11-27 10:26:40 -0500

    Expiration Month and Year

--- src/components/Checkout/Billing/Payment.js
+++ src/components/Checkout/Billing/Payment.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { resetLandingCache } from "../../../redux/landing/landingSearchSlice";
 import { useNewClientPmtMutation } from "../../../redux/checkout/checkoutApiSlice";
+import {useFormik} from "formik";
 import "./Payment.css";
 
 const Payment = () => {
@@ -12,7 +13,9 @@ const Payment = () => {
   //Array[] space_id, final_price, check_in time, check_out time
   const resInfo = useSelector((state) => state.reservation);
   const [cardNumber, setCardNumber] = useState("");
-  const [expiryDate, setExpiryDate] = useState("");
+  const [expiryDate, setExpiryDate] = useState('')
+  const [expiryMonth, setExpiryMonth] = useState('');
+  const [expiryYear, setExpiryYear] = useState('');
   const [cvv, setCvv] = useState("");
   const [nameOnCard, setNameOnCard] = useState("");
   const [err, setErr] = useState(false);
@@ -20,9 +23,52 @@ const Payment = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const formik = useFormik({
+    initialValues: {
+      cardNumber: '',
+      expiryMonth: '',
+      expiryYear: '',
+      cvv: '',
+      nameOnCard: '',
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.cardNumber) {
+        errors.cardNumber = 'Required';
+      } else if (!/^\d{16}$/.test(values.cardNumber)) {
+        errors.cardNumber = 'Invalid card number';
+      }
+
+      if (!values.expiryMonth) {
+        errors.expiryMonth = 'Required';
+      }
+
+      if (!values.expiryYear) {
+        errors.expiryYear = 'Required';
+      }
+
+      if (!values.cvv) {
+        errors.cvv = 'Required';
+      } else if (!/^\d{3,4}$/.test(values.cvv)) {
+        errors.cvv = 'Invalid CVV';
+      }
+
+      if (!values.nameOnCard) {
+        errors.nameOnCard = 'Required';
+      }
+      console.log(errors)
+    },
+    onSubmit: (e) => {
+      console.log("worked?")
+      setExpiryDate(`${expiryMonth}/${expiryYear}`)
+      handlePaymentSubmit(e)
+   },
+  });
+
   const handlePaymentSubmit = async (e) => {
-    e.preventDefault();
-    if ([cardNumber, expiryDate, cvv, nameOnCard].some((item) => item === "")) {
+    console.log("this?")
+    if ([cardNumber, expiryDate, cvv, nameOnCard].some(item => item === "")) {
       return;
     }
     await newClientPmt({ data: [expiryDate, resInfo.booking_id] })
@@ -30,12 +76,35 @@ const Payment = () => {
       .then((res) => {
         if (!res.success) {
           setErr(true);
-          return;
+          return
         }
         dispatch(resetLandingCache());
-        navigate(`/client/pmt/success/${resInfo.nav_id}/${res.pmt_id}`);
-      })
-      .catch((e) => console.error(e));
+        navigate(`/client/pmt/sucess/${resInfo.nav_id}/${res.pmt_id}`);
+      }).catch(e => console.error(e));
+  };
+
+  const handleCardNumberChange= (event) => {
+    setCardNumber(event.target.value);
+    formik.handleChange(event);
+  };
+
+  const handleExpiryMonthChange = (event) => {
+    setExpiryMonth(event.target.value);
+    formik.handleChange(event);
+  };
+
+  const handleExpiryYearChange = (event) => {
+    setExpiryYear(event.target.value);
+    formik.handleChange(event);
+  };
+  const handleCvvChange = (event) => {
+    setCvv(event.target.value);
+    formik.handleChange(event);
+  };
+
+  const handleNameChange = (event) => {
+    setNameOnCard(event.target.value);
+    formik.handleChange(event);
   };
 
   return (
@@ -59,14 +128,16 @@ const Payment = () => {
       </div>
 
       {/* Payment form */}
-      <form className="payment-form" onSubmit={handlePaymentSubmit}>
+      <form className="payment-form" onSubmit={formik.handleSubmit}>
         <label>
           <span className="label-card-name">Name on Card</span>
           <br />
           <input
             type="text"
+            id="nameOnCard"
             value={nameOnCard}
-            onChange={(e) => setNameOnCard(e.target.value)}
+            onChange={handleNameChange}
+            onBlur={formik.handleBlur}
           />
         </label>
         <label>
@@ -74,27 +145,81 @@ const Payment = () => {
           <br />
           <input
             type="text"
+            id="cardNumber"
             value={cardNumber}
-            onChange={(e) => setCardNumber(e.target.value)}
+            onChange={handleCardNumberChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.touched.cardNumber && formik.errors.cardNumber ? (
+          <div style={{ color: 'red' }}>{formik.errors.cardNumber}</div>
+        ) : null}
         </label>
         <label>
           <span className="label-card-exp">Expiration Date</span>
           <br />
-          <input
-            type="text"
-            value={expiryDate}
-            onChange={(e) => setExpiryDate(e.target.value)}
-          />
+          <select 
+            id="expiryMonth"
+            onChange={handleExpiryMonthChange}
+            onBlur={formik.handleBlur}
+            value={expiryMonth}
+          >
+            <option value="null">--</option>
+            <option value="2">02</option>
+            <option value="3">03</option>
+            <option value="4">04</option>
+            <option value="5">05</option>
+            <option value="6">06</option>
+            <option value="7">07</option>
+            <option value="8">08</option>
+            <option value="9">09</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+          {formik.touched.expiryMonth && formik.errors.expiryMonth ? (
+          <div style={{ color: 'red' }}>{formik.errors.expiryMonth}</div>
+        ) : null}
+          <select 
+          id="expiryYear" 
+          onChange={handleExpiryYearChange}
+          onBlur={formik.handleBlur}
+          value={expiryYear}
+          >
+            <option value="null">--</option>            
+            <option value="23">23</option>
+            <option value="24">24</option>
+            <option value="25">25</option>
+            <option value="26">26</option>
+            <option value="27">27</option>
+            <option value="28">28</option>
+            <option value="29">29</option>
+            <option value="30">30</option>
+            <option value="31">31</option>
+            <option value="32">32</option>
+            <option value="33">33</option>
+            <option value="34">34</option>
+            <option value="35">35</option>
+            <option value="36">36</option>
+            <option value="37">37</option>
+            <option value="38">38</option>
+            <option value="39">39</option>
+            <option value="40">40</option>
+            <option value="41">41</option>
+            <option value="42">42</option>
+            <option value="43">43</option>
+          </select>
         </label>
+        <br />
 
         <label>
           <span className="label-card-cvv">CVV (security code)</span>
           <br />
           <input
             type="text"
+            id="cvv"
             value={cvv}
-            onChange={(e) => setCvv(e.target.value)}
+            onChange={handleCvvChange}
+            onBlur={formik.handleBlur}
           />
         </label>
         <br />
@@ -106,4 +231,7 @@ const Payment = () => {
   );
 };
 
+
 export default Payment;
+
+
