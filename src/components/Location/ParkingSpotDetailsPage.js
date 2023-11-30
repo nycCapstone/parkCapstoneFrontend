17dda85e11dfc2ac2cf3a549173b03e152af84a9
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-11-30 12:31:18 -0500

    new look

--- src/components/Location/ParkingSpotDetailsPage.js
+++ src/components/Location/ParkingSpotDetailsPage.js
@@ -26,20 +26,20 @@ function ParkingSpotDetailPage() {
         <div className="details-container">
           {/* Details Information */}
           <div className="title">
-            <h1>Parking Spot Details</h1>
+            <h1>Address: {spotDetails.prop_address}</h1>
           </div>
 
-          <div className="details">
+          {/* <div className="details">
             <p className="detail-label">Address:</p>
             <p className="detail-value">{spotDetails.prop_address}</p>
-          </div>
+          </div> */}
           <div className="details">
             <p className="detail-label">Number of Spaces:</p>
             <p className="detail-value">{spotDetails.number_spaces}</p>
           </div>
           <div className="details">
             <p className="detail-label">Billing Type:</p>
-            <p className="detail-value">{spotDetails.billing_type}</p>
+            <p className="detail-value">{spotDetails.billing_type}/daily</p>
           </div>
           <div className="details">
             <p className="detail-label">Owner ID:</p>
