1b4fb99f4284bc3fe1f8741b2bd9c5288d008513
YasserS-21 <saadi.yasser04@gmail.com>
2023-12-06 11:55:22 -0500

    Transactions Cards

--- src/components/Client/ClientBookings.js
+++ src/components/Client/ClientBookings.js
@@ -5,77 +5,88 @@ import { calculateDateDifferenceInDays } from "../../constants/helper/helper";
 import SearchLoading from "../../assets/Spinners/SearchLoading";
 import { FaEdit } from "react-icons/fa";
 import EditStars from "./Views/EditStars";
+import {Link} from "react-router-dom"
 import "./Styles/BookingsComponent.css";
 
+
 const ClientBookings = () => {
-  const {
-    data: bookings,
-    isSuccess,
-    isLoading,
-    error,
-    refetch,
-  } = useGetBookingsQuery();
-  const { data: userData } = useGetUserInfoQuery();
-  const [show, setShow] = useState(null);
+ const {
+   data: bookings,
+   isSuccess,
+   isLoading,
+   error,
+   refetch,
+ } = useGetBookingsQuery();
+ const { data: userData } = useGetUserInfoQuery();
+ const [show, setShow] = useState(null);
 
-  useEffect(() => {
-    if (isSuccess) {
-      if (userData?.id !== bookings[0]?.customer_booking_id) {
-        refetch();
-      }
-    }
-  }, []);
 
-  if (isLoading) {
-    return (
-      <div className="s-loading-container">
-        <SearchLoading />
-      </div>
-    );
-  }
-  if (error) {
-    return <div>Api Down</div>;
-  }
+ useEffect(() => {
+   if (isSuccess) {
+     if (userData?.id !== bookings[0]?.customer_booking_id) {
+       refetch();
+     }
+   }
+ }, []);
 
-  if (isSuccess) {
-    return (
-      <div className="bookings-container">
-        {!bookings.length && <div>No bookings made yet</div>}
-        {bookings.length > 0 && (
-          <>
-            <header className="m-bookings-header">
-              <h2>Your bookings</h2>
-            </header>
-            <div className="bookings-grid-container">
-              {bookings.map((booking, i) => (
-                <div key={booking.booking_id} className="booking-item">
-                  <p>Booking ID: {booking.booking_id}</p>
-                  <p>Active: {booking.is_occupied ? "Yes" : "No"}</p>
-                  <p>
-                    Start Time: {new Date(booking.start_time).toLocaleString()}
-                  </p>
-                  <p>End Time: {new Date(booking.end_time).toLocaleString()}</p>
-                  <p>Final Cost: ${booking.final_cost}</p>
-                  <div style={{display: "flex", flexWrap: "wrap"}}>
-                  <p>Rating: {booking.rating}</p>
-                  {
-                    calculateDateDifferenceInDays(booking.end_time) < 13 && <FaEdit style={{marginLeft: "1rem", cursor: "pointer"}} onClick={() => {
-                      setShow(i)
-                    }}/>
-                  }
-                  {
-                    show === i && <EditStars booking={booking} setShow={setShow} refetch={refetch}/>
-                  }
 
-                  </div>
-                </div>
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    );
-  }
+ if (isLoading) {
+   return (
+     <div className="s-loading-container">
+       <SearchLoading />
+     </div>
+   );
+ }
+ if (error) {
+   return <div>Api Down</div>;
+ }
+console.log(bookings)
+ if (isSuccess) {
+   return (
+     <div className="bookings-container">
+       {!bookings.length && <div>No bookings made yet</div>}
+       {bookings.length > 0 && (
+         <>
+           <header className="m-bookings-header">
+             <h2>Your bookings</h2>
+           </header>
+           <div className="bookings-grid-container">
+             {bookings.map((booking, i) => (
+               <div key={booking.booking_id} className="booking-item">
+                 <p className="booking-label">Order Number - {booking.booking_id}</p>
+                 <p>Active: {booking.is_occupied ? "Yes" : "No"}</p>
+                 <p>
+                   Start Time: {new Date(booking.start_time).toLocaleString()}
+                 </p>
+                 <p>End Time: {new Date(booking.end_time).toLocaleString()}</p>
+                 <p>Final Cost: ${booking.final_cost}</p>
+                 <div style={{display: "flex", flexWrap: "wrap"}}>
+                 <p>Rating: {booking.rating}</p>
+
+
+                 {
+                   calculateDateDifferenceInDays(booking.end_time) < 13 && <FaEdit style={{marginLeft: "1rem", cursor: "pointer"}} onClick={() => {
+                     setShow(i)
+                   }}/>
+                 }
+                 {
+                   show === i && <EditStars booking={booking} setShow={setShow} refetch={refetch}/>
+                 }
+                 <Link to={`/parking-spots/${booking.booking_space_id}`}>
+                       <button className="show-more-button">View Details</button>
+                 </Link>
+                 </div>
+               </div>
+             ))}
+           </div>
+         </>
+       )}
+     </div>
+   );
+ }
 };
 
+
 export default ClientBookings;
+
+
--- src/components/Client/MyActivity.js
+++ src/components/Client/MyActivity.js
@@ -4,58 +4,65 @@ import { FaChevronCircleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Styles/MyActivity.css";
 
+
 const MyActivity = () => {
-  const {
-    data: activity,
-    isSuccess,
-    isLoading,
-    error,
-    isUninitialized,
-  } = useGetClientTransactionsQuery({}, { refetchOnMountOrArgChange: true });
+ const {
+   data: activity,
+   isSuccess,
+   isLoading,
+   error,
+   isUninitialized,
+ } = useGetClientTransactionsQuery({}, { refetchOnMountOrArgChange: true });
 
-  if (isLoading || isUninitialized) {
-    return <div className="s-loading-container"><SearchLoading /></div>;
-  }
 
-  if (isSuccess) {
-    return (
-      <div className="myactivity-container">
-        <header>
-        <div className="cl-h-svgleft">
-          <Link to="/admin">
-            <FaChevronCircleLeft />
-          </Link>
-        </div>
-          <h2>Most Recent bookings paid for</h2>
-        </header>
-        {activity?.length > 0 ? (
-          <div className="payment-list">
-            {activity.map((item, idx) => {
-              return (
-                <div key={idx} className="payment-list-item">
-                  <i>{idx + 1}</i>
-                  <p>Pmt Id: {item.pmt_id}</p>
-                  <p>Card Expires: {item.expiry}</p>
-                  <p>Booking Id: {item.pmt_booking_id}</p>
-                  <p>
-                    Pmt made @: {new Date(item.timestamp).toLocaleDateString()}
-                  </p>
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div className="no-activity" style={{marginLeft: "2rem"}}>
-            You currently do not have any transactions yet!
-          </div>
-        )}
-      </div>
-    );
-  }
+ if (isLoading || isUninitialized) {
+   return <div className="s-loading-container"><SearchLoading /></div>;
+ }
+ if (isSuccess) {
+   return (
+     <div className="myactivity-container">
+       <header>
+       <div className="cl-h-svgleft">
+         <Link to="/admin">
+           <FaChevronCircleLeft />
+         </Link>
+       </div>
+         <h2>Most Recent bookings paid for</h2>
+       </header>
+       {activity?.length > 0 ? (
+         <div className="payment-list">
+           {activity.map((item, idx) => {
+             console.log(item)
+             return (
+               <div key={idx} className="payment-list-item">
+                 <i>{idx + 1}</i>
+                 <p className="payment-label">Payment Id:</p>
+                 <p>{item.pmt_id}</p>
+                 <p>Card Expires: {item.expiry}</p>
+                 <p>Booking Id: {item.pmt_booking_id}</p>
+                 <p>
+                   Pmt made @: {new Date(item.timestamp).toLocaleDateString()}
+                 </p>
+               </div>
+             );
+           })}
+         </div>
+       ) : (
+         <div className="no-activity" style={{marginLeft: "2rem"}}>
+           You currently do not have any transactions yet!
+         </div>
+       )}
+     </div>
+   );
+ }
 
-  if (error) {
-    return <div>Api Down</div>;
-  }
+
+ if (error) {
+   return <div>Api Down</div>;
+ }
 };
 
+
 export default MyActivity;
+
+
