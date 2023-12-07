dcd28ee7280838b3f705dfc1ff285650f3c4123d
Jibon P <jibonp@jibons-air.mynetworksettings.com>
2023-12-07 12:26:44 -0500

    fixed some issues

--- src/components/Location/ParkingSpotDetailsPage.js
+++ src/components/Location/ParkingSpotDetailsPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useGetOneSpotQuery } from "../../redux/client/searchApiSlice";
 import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
