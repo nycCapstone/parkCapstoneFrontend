e6232b57d0a1b8d3bd86471359a9cde22fc36f9a
YasserS-21 <saadi.yasser04@gmail.com>
2023-12-21 13:32:13 -0500

    Admin Page Updates

--- src/components/Dashboard/Admin.js
+++ src/components/Dashboard/Admin.js
@@ -26,40 +26,9 @@ const Admin = () => {
       </div>
     );
   }
-
   if (isSuccess) {
     return (
-      <div className="admin-all-container">
-        <h1 className="welcome-heading">
-          Welcome,{" "}
-          {data?.first_name && (
-            <span className="profile-welcome-first-name">
-              {data?.first_name[0].toUpperCase() +
-                data.first_name.slice(1).toLowerCase()}
-            </span>
-          )}
-        </h1>
-        <AdminPage />
-        <section className="home-container">
-          <div className="link-container">
-            <Link to="/client" className="nav-link">
-              Go to the Client page
-            </Link>
-            <Link to="/client/transactions" className="nav-link">
-              Go to My Transactions
-            </Link>
-            <Link to="/renter" className="nav-link">
-              Go to the Renter
-            </Link>
-          </div>
-
-          <div className="flexGrow">
-            <button className="sign-out-button" onClick={signOut}>
-              Sign Out
-            </button>
-          </div>
-        </section>
-      </div>
+      <AdminPage/>
     );
   }
 
