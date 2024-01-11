0254ff2d5b22ca1bde90ff7d86b5fa693ce6e2b8
zazimov <zalmanazimov@pursuit.org>
2024-01-11 13:46:29 -0500

    fixed images on profile and additional profile css changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,9 +7,17 @@ import appLogo from "./appLogo.png";
 import ori_logo from "./ori_logo.png";
 import logo_final from "./logo_final.png";
 import Isabelle_profile from "./Isabelle_profile.png";
+import Isabelle_square from "./Isabelle_square.png";
+import Isabelle_rectang from "./Isabelle_rectang.png";
 import Michael_profile from "./Michael_profile.png";
+import Michael_square from "./Michael_square.png";
+import Michael_rectang from "./Michael_rectang.png";
 
 export {
+  Michael_rectang,
+  Michael_square,
+  Isabelle_rectang,
+  Isabelle_square,
   Michael_profile,
   Isabelle_profile,
   carvaletlogo,
